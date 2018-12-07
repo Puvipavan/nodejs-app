c04485aa51ae1f83570e79a15db6d2dcb56d5234
Puvipavan Palendrarajah <14966528+Puvipavan@users.noreply.github.com>
2018-12-07 19:59:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 80;
+
+let app = express();
+
+function getOutput(req){
+    let message = process.env.MESSAGE;
+    return "<h1>"+output+"</h1>";
+}
+app.get('/*', function(req, res){
+    res.send(getOutput(req));
+});
+
+app.listen(PORT, function(err){
+    if(err){
+        console.log("Unable to listen on PORT "+ PORT);
+        process.exit(-1);
+    }
+    console.log('Server Started on PORT :'+ PORT);
+});
